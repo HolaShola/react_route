909611e1a5f3289d585671e0fb164e60eb24083b
Yauheni_Sholakhau <Yauheni_Sholakhau@epam.com>
2017-06-08 15:30:31 +0300

    add URL params

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, hashHistory, Link } from 'react-router';
+import { Router, Route, hashHistory, Link, IndexRoute } from 'react-router';
 
 import Component_1 from './components/component_1';
 import NotFound from './components/not_found';
 import Component_2 from './components/component_2';
 import Component_3 from './components/component_3';
+import Repo from './components/repo';
 
-function App() {
+function App(props) {
     return (
         <div>
             Hello hello
+            <Link to="/component1">Component1</Link>
+            <Link to="/component2">Component2</Link>
+            <Link to="/component3">Component3</Link>
+            {props.children}
         </div>
     );
 }
 
 ReactDOM.render((
     <Router history={hashHistory}>
-        <Route path="/" component={App} />
-        <Route path="/component1" component={Component_1} />
-        <Route path="/component2" component={Component_2} />
-        <Route path="/component3" component={Component_3} />
+        <Route path="/" component={App}>
+            <Route path="/component1" component={Component_1} />
+            <Route path="/component2" component={Component_2} />
+            <Route path="/component2/:id" component={Repo} />
+            <Route path="/component3" component={Component_3} />
+
+        </Route>
         <Route path="*" component={NotFound} />
     </Router>),
     document.querySelector('#root')
